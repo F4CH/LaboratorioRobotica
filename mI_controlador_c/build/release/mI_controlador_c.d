453be875e0d43c0dfcdaa6e370ee1b40db9032d7
F4CH <161516529+F4CH@users.noreply.github.com>
2026-04-04 12:45:46 -0300

    Delete mI_controlador_c directory

--- mI_controlador_c/build/release/mI_controlador_c.d
+++
@@ -1,5 +0,0 @@
-build/release/mI_controlador_c.o: mI_controlador_c.c \
- C:\Program\ Files\Webots/include/controller/c/webots/robot.h \
- C:\Program\ Files\Webots/include/controller/c/webots/types.h \
- C:\Program\ Files\Webots/include/controller/c/webots/nodes.h \
- C:\Program\ Files\Webots/include/controller/c/webots/motor.h
