d97f83bb2bdda1d33a68d242f4671914a42848df
F4CH <161516529+F4CH@users.noreply.github.com>
2026-04-04 12:40:27 -0300

    Add files via upload
    lo que se tiene que hacer con esos archivos es dejarlo en la carpeta del e-puck/controllers
    
    por ejemplo en mi pc es aqui 
    C:\Users\benja\OneDrive\Documentos\e-puck\controllers

---
+++ mI_controlador_c/build/release/mI_controlador_c.d
@@ -0,0 +1,5 @@
+build/release/mI_controlador_c.o: mI_controlador_c.c \
+ C:\Program\ Files\Webots/include/controller/c/webots/robot.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/types.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/nodes.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/motor.h
