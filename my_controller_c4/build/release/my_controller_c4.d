cc2b2ce66cbef1e46160603716482e20aa8ca476
F4CH <161516529+F4CH@users.noreply.github.com>
2026-04-05 19:05:33 -0400

    Delete my_controller_c4 directory

--- my_controller_c4/build/release/my_controller_c4.d
+++
@@ -1,5 +0,0 @@
-build/release/my_controller_c4.o: my_controller_c4.c \
- C:\Program\ Files\Webots/include/controller/c/webots/robot.h \
- C:\Program\ Files\Webots/include/controller/c/webots/types.h \
- C:\Program\ Files\Webots/include/controller/c/webots/nodes.h \
- C:\Program\ Files\Webots/include/controller/c/webots/motor.h
